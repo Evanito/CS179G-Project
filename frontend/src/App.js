1a80e8039213b61c39e0344c250a344b7f8ec3b9
Ronan <rtodd001@ucr.edu>
2020-11-09 20:29:13 -0800

    updated the post to display image and name from the server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from "react-apollo"
+
 import Header from './components/Header';
 import Post from './components/Posts';
 import images from './Images/sample.png';
 import av from './Images/avatar.png';
+let serverName = "http://evpi.nsupdate.info:14200/user";
+
+const client = new ApolloClient({
+  uri: serverName
+})
 
 class  App extends Component{
   render(){
     return (
-      <div className="App">
-        <Header/>
-        <section className="App-Feed">
-          <Post name="123" avatar={av} image={images} caption="car"/>
-          <Post name="123" avatar={av} image={images} caption="car"/>
-        </section>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header/>
+          <section className="App-Feed">
+            <Post/>
+          </section>
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- frontend/src/components/Posts/index.js
+++ frontend/src/components/Posts/index.js
@@ -1,38 +1,85 @@
 import "./Post.css"
+import React, { Component, useRef } from "react";
+import {Query} from "react-apollo";
+import gql from "graphql-tag"
+import av from '../../Images/avatar.png';
+import sp from '../../Images/sample.png';
+import { render } from "@testing-library/react";
+const avatar = av;
+//const image = sp;
+const caption = "caption";
+let result = []
+let serverName = "http://evpi.nsupdate.info:14200/";
 
-import React, { Component } from "react";
-class Post extends Component {
+function getName(){
+    let endpoint = 'user?'
+    return fetch(serverName + endpoint + 'id=696969').then((response) => response.json())
+    .then(data => data.data[0].username)
+}
+
+function getImage(){
+    let endpoint = 'image?'
+    return fetch(serverName + endpoint + 'id=696969').then((response) => response.blob())
+    .then(data => data)
+}
+
+function getAll(){
+    return Promise.all([getName(), getImage()])
+}
+
+class Post extends React.Component {
+    name;
     constructor(props){
-        super(props);
+        super(props)
+        this.state = {
+            name: null,
+            image: null,
+        };
+        getAll()
+            .then(([username, userImage]) => {
+                this.setState({name: username});
+                this.setState({image:URL.createObjectURL(userImage)})
+                console.log("name ",username)
+                console.log("Image ", userImage)
+            })
     }
-    render() {
-        const image = this.props.image;
-        const avatar = this.props.avatar;
-        const name = this.props.name;
-        const caption = this.props.caption;
-
+    render(){
+        /* async function getPosts() {   
+            let url = new URL(serverName);
+            let params = {
+                'id':696969,
+            }
+            url.search = new URLSearchParams(params).toString();
+            let response = fetch(url)
+            .then(response => response.json())
+            .then(data => result = data)
+            console.log("response ",response)
+            console.log("result ",result)
+            console.log("DATA ",result)
+        } */
         return(
             <article className="Post" ref="Post">
-            <header>
-                <div className="Post-user">
-                <div className="Post-user-avatar">
-                    <img src={avatar} alt={name} />
+                <header>
+                    <div className="Post-user">
+                    <div className="Post-user-avatar">
+                        <img src={avatar} alt={this.state.name} />
+                    </div>
+                    <div className="Post-user-nickname">
+                        <span>{this.state.name}</span>
+                    </div>
+                    </div>
+                </header>
+                <div className="Post-image">
+                    <div className="Post-image-bg">
+                    <img alt={caption} src={this.state.image} />
+                    </div>
                 </div>
-                <div className="Post-user-nickname">
-                    <span>{name}</span>
+                <div className="Post-caption">
+                    <strong>{this.state.name}</strong> {caption}
                 </div>
-                </div>
-            </header>
-            <div className="Post-image">
-                <div className="Post-image-bg">
-                <img alt={caption} src={image} />
-                </div>
-            </div>
-            <div className="Post-caption">
-                <strong>{name}</strong> {caption}
-            </div>
-            </article>
-            )
-        }
-}
+        </article>
+        );
+    }
+    
+ }
 export default Post;
