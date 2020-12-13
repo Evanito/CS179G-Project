74380431e41b1db1ac19bd70e4c4789e5c3756b2
mwals003 <56177640+mwals003@users.noreply.github.com>
2020-12-13 15:01:32 -0800

    Added Client ID/Secret to run.sh

--- auth/run.sh
+++ auth/run.sh
@@ -1,10 +1,10 @@
 export FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
 export FN_BASE_URI=http://localhost:8040
-export FN_CLIENT_ID=THE CLIENT ID WHICH YOU CREATED EARLIER
-export FN_CLIENT_SECRET=THE CLIENT SECRET WHICH YOU CREATED EARLIER
+export FN_CLIENT_ID=820186523469-4hhhse7jujgedn0rnood11turbppur5u.apps.googleusercontent.com
+export FN_CLIENT_SECRET=CRbeElS_QnzKbcrFcLFKZRZn
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
-export FN_FLASK_SECRET_KEY=SOMETHING RANDOM AND SECRET
+export FN_FLASK_SECRET_KEY=grangran
 
 python -m flask run -p 8040
